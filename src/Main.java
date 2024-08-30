3ab8c7824d94cd8776f5b6ccfa74d37105b897bc
JoaquinSabater <89492839+JoaquinSabater@users.noreply.github.com>
2024-08-29 23:11:59 -0300

    No puedo correr los testers

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import AnalizadorLexico.*;
 import sourcemanager.*;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +12,16 @@ public class Main {
 
             gestorDeArchivos = new SourceManagerImpl();
             AL = new analizadorLexico(gestorDeArchivos);
+
+            gestorDeArchivos.open(args[0]);
+
+            while (!Objects.equals(AL.proximoToken().getToken_id(), "eof")){
+                AL.proximoToken();
+            }
+
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ExcepcionLexica e) {
             throw new RuntimeException(e);
         }
     }
