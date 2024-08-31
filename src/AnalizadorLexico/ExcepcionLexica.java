c741eddcfca0d66b7f1cd519823b117395863094
JoaquinSabater <89492839+JoaquinSabater@users.noreply.github.com>
2024-08-30 21:55:25 -0300

    Exepcion lexica terminada

--- src/AnalizadorLexico/ExcepcionLexica.java
+++ src/AnalizadorLexico/ExcepcionLexica.java
@@ -2,12 +2,12 @@ package AnalizadorLexico;
 
 public class ExcepcionLexica extends Exception {
     public ExcepcionLexica(int numeroLinea, int numeroColumna, String lexema, String tipoError, String lineaContenido) {
-        super(formatearMensaje(numeroLinea, numeroColumna, lexema, tipoError, lineaContenido));
+        super("\n" + formatearMensaje(numeroLinea, numeroColumna, lexema, tipoError, lineaContenido));
     }
 
     private static String formatearMensaje(int numeroLinea, int numeroColumna, String lexema, String tipoError, String lineaContenido) {
         StringBuilder mensaje = new StringBuilder();
-        mensaje.append("Error Léxico en linea ").append(numeroLinea).append(": ").append(lexema).append(" ").append(tipoError).append("\n");
+        mensaje.append("Error Léxico en linea ").append(numeroLinea).append(", columna ").append(numeroColumna).append(": ").append(tipoError).append("\n");
         mensaje.append("Detalle: ").append(lineaContenido).append("\n");
         mensaje.append(" ".repeat("Detalle: ".length() + numeroColumna - 1)).append("^\n");
         mensaje.append("[Error:").append(lexema).append("|").append(numeroLinea).append("]");
